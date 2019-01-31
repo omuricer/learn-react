62842d7f5afdea25e09acd8a553fccc590e40747
sho-inada <sho-inada@usen.co.jp>
2019-01-31 16:48:52 +0900

    typescript

---
+++ web/app/build/precache-manifest.0bce13f6c668bdccd144091d4b0d98cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1bfff28274801e753e4f",
+    "url": "/static/js/main.1bfff282.chunk.js"
+  },
+  {
+    "revision": "ec8eef1e198ebb0cbe4b",
+    "url": "/static/js/1.ec8eef1e.chunk.js"
+  },
+  {
+    "revision": "1bfff28274801e753e4f",
+    "url": "/static/css/main.c6b4be41.chunk.css"
+  },
+  {
+    "revision": "76385c5dd961fc63bd2af892a0ed296a",
+    "url": "/index.html"
+  }
+];
--- web/app/src/index.tsx
+++ web/app/src/index.tsx
@@ -1,11 +1,152 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './index.css';
-import registerServiceWorker from './registerServiceWorker';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+type SquareType = string | null;
+interface SquareProps {
+  value: SquareType;
+  onClick: () => void;
+}
+function Square(props: SquareProps) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  )
+}
+
+class Board extends React.Component<any> {
+  renderSquare(i: number) {
+    return <Square value={this.props.squares[i]} onClick={() => this.props.onClick(i)}/>;
+  }
+  render() {
+    return (
+      <div>
+        {/* <div className="status">{status}</div> */}
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+interface BoardState {
+  squares: Array<SquareType>;
+}
+interface GameState {
+  history: Array<BoardState>;
+  stepNumber: number;
+  xIsNext: boolean;
+}
+class Game extends React.Component<any, GameState> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      history: [{
+        squares: Array<SquareType>(9).fill(null),
+      }],
+      stepNumber: 0,
+      xIsNext: true,
+    }
+  }
+  handleClick(i: number) {
+    var history = this.state.history;
+    var current = history[history.length - 1];
+    const squares = current.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      history: history.concat([{
+        squares: squares
+      }]),
+      stepNumber: history.length,
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+  jumpTo(step: number) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) ? false : true,
+    });
+  }
+  render() {
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[this.state.stepNumber];
+    const winner = calculateWinner(current.squares);
+    let status;
+    if (winner) {
+      status = 'Winner is ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext? 'X' : 'O');
+    }
+    const moves = history.map((step, move) => {
+      const desc = move ?
+        'Move #' + move :
+        'Game start';
+      return (
+        <li key={move}>
+          <a href="#" onClick={() => this.jumpTo(move)}>{desc}</a>
+        </li>
+      );
+    });
+
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board
+            squares={current.squares}
+            onClick={(i: number) => this.handleClick(i)}
+          />
+        </div>
+        <div className="game-info">
+          <div>{status}</div>
+          <ol>{moves}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
+  <Game />,
+  document.getElementById('container')
 );
-registerServiceWorker();
+
+function calculateWinner(squares: SquareType[]) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
